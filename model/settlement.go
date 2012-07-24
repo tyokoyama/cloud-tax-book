96c6110c7a0d10959583e5f596feda6b43ba4328
Takashi Yokoyama <tksyokoyama@gmail.com>
2012-07-24 18:45:01 +0900

    経費のまとめ機能の作成開始。 （まだ、Typeのみ実装）

---
+++ model/settlement.go
@@ -0,0 +1,31 @@
+/*
+Copyright 2012 Takashi Yokoyama
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+package model
+
+/*
+import (
+	"appengine"
+	"appengine/datastore"
+)
+*/
+
+type Settlement struct {
+	TypeId int64 `datastore:",noindex"` 	// 勘定科目種別
+	IsExpense bool `datastore:",noindex"`	// 経費かどうか（true：経費／false：経費ではない）
+	MoneyIn int64 `datastore:",noindex"`	// 収入
+	MoneyOut int64 `datastore:",noindex"`	// 支出
+}
+
